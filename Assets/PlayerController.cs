6bec8096e66c6e8f438c1c0ffb8dd99778315bd2
raimonds <raimonds.udris@anatomynext.com>
2019-01-26 00:22:37 +0200

    Spawner

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
-    public float speed;
+	Rigidbody rb;
+	public float speed;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-    void FixedUpdate()
-    {
-        float mH = Input.GetAxis("Horizontal");
-        float mV = Input.GetAxis("Vertical");
-        rb.velocity = new Vector3(mH * speed, rb.velocity.y, mV * speed);
-    }
-    private void Update()
-    {
-        transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
-    }
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate()
+	{
+		float mH = Input.GetAxis("Horizontal");
+		float mV = Input.GetAxis("Vertical");
+
+		Vector3 move = Camera.main.transform.rotation * new Vector3(mH, 0, mV) * speed;
+		move.y = rb.velocity.y;
+		rb.velocity = move;
+	}
+	private void Update()
+	{
+		transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+	}
 }
 
 
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	[Range(0,5)]
+	public float interval = 2;
+
+	public GameObject[] spawnPrefabs;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		InvokeRepeating("SpawnPrimitive", 0, interval);
+	}
+
+	// Update is called once per frame
+	void SpawnPrimitive()
+	{
+		Vector3 pos = new Vector3(Random.Range(1f, 6f), transform.position.y, Random.Range(1f, 4f));
+		int prfab = Random.Range(0, spawnPrefabs.Length);
+
+		Instantiate(spawnPrefabs[prfab], pos, Quaternion.identity);
+	}
+}
