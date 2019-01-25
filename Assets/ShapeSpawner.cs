822350b151572bb538e6bf0afd238a3122011f57
Ingus Betmans <ingus.betmans@gmail.com>
2019-01-25 21:47:36 +0200

    ShapeSpawner

--- Assets/ShapeSpawner.cs
+++ Assets/ShapeSpawner.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ShapeSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject primitiveCube;
+
     void Start()
     {
-        
+        InvokeRepeating("SpawnPrimitive", 0, 3);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnPrimitive(GameObject primitive)
     {
-        
+        GameObject currPrimitive = Instantiate(primitive);
+        Vector3 primitiveLocalScale = currPrimitive.transform.localScale; 
+        currPrimitive.transform.localScale = ResizePrimitive();
+        currPrimitive.name = "Primitive";
+        currPrimitive.transform.position = new Vector3(0, 10, 0);
+
+    }
+
+    private Vector3 ResizePrimitive()
+    {
+        return new Vector3(Random.Range(1,5), Random.Range(1,5), Random.Range(1,5));
     }
 }
