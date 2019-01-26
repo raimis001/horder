6ddd53061409996b3f86157319b7a04153ef85c1
raimonds <raimonds.udris@anatomynext.com>
2019-01-26 20:51:11 +0200

    Asset

---
+++ Assets/Scripts/CollisionItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionItem : BaseItem
+{
+	protected override void ExplodeItem()
+	{
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		
+	}
+}
