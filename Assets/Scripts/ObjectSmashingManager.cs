6b82cb1c8c7b8ad4a6eb1f54bb29935afd969ec5
raimonds <raimonds.udris@anatomynext.com>
2019-01-27 11:37:50 +0200

    Physics

--- Assets/Scripts/ObjectSmashingManager.cs
+++ Assets/Scripts/ObjectSmashingManager.cs
@@ -5,52 +5,53 @@ using UnityEngine;
 public class ObjectSmashingManager : BaseItem
 {
 	public AudioSource sound;
-    public GameObject smashedObj;
+	public GameObject smashedObj;
 
-    private void FixedUpdate()
-    {
-        if (!itemPicked && itemThrown && GetComponent<Rigidbody>().velocity.magnitude <= 0.000001f)
-        {
+	private void FixedUpdate()
+	{
+		if (!itemPicked && itemThrown && GetComponent<Rigidbody>().velocity.magnitude <= 0.000001f)
+		{
 			Debug.Log("Close trow");
-						//itemThrown = false; //Item has settled
-        }
-    }
+			//itemThrown = false; //Item has settled
+		}
+	}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-		Debug.Log(GetComponent<Rigidbody>().velocity.magnitude);
+	private void OnCollisionEnter(Collision collision)
+	{
 		if (itemThrown)
-        {
-			Debug.Log(GetComponent<Rigidbody>().velocity.magnitude);
-            if (GetComponent<Rigidbody>().velocity.magnitude > 3.5f)
-            {
-                SmashItemMesh(true);
-            }
+		{
+			if (GetComponent<Rigidbody>().velocity.magnitude > 2.5f)
+			{
+				GetComponent<Rigidbody>().velocity = Vector3.zero;
 
-            if (collision.collider.GetComponent<ObjectSmashingManager>())
-            {
-                collision.collider.GetComponent<ObjectSmashingManager>().SmashItemMesh(true);
-            }
+				SmashItemMesh(true);
+			}
+
+			if (collision.collider.GetComponent<ObjectSmashingManager>())
+			{
+				collision.collider.GetComponent<ObjectSmashingManager>().SmashItemMesh(true);
+			}
 			itemThrown = false;
 
-				}
-    }
+		}
+	}
 
-    public void SmashItemMesh(bool addExplosion)
-    {
-        Debug.Log("Smashing");
-        if (!smashedObj) return;
+	public void SmashItemMesh(bool addExplosion)
+	{
+		Debug.Log("Smashing");
+		if (!smashedObj) return;
 
-        Instantiate(smashedObj, transform.position, transform.rotation);
-        if (addExplosion)
-        {
-            foreach (Transform item in smashedObj.transform)
-            {
-                item.GetComponent<Rigidbody>().AddForce(Random.Range(-4f, 4f), Random.Range(-4f, 4f), Random.Range(-4f, 4f), ForceMode.Impulse);
-            }
-        }
-				if (sound) sound.Play();
+		GameObject t = Instantiate(smashedObj, transform.position, transform.rotation);
+		if (addExplosion)
+		{
+			foreach (Transform item in t.transform)
+			{
+				item.GetComponent<Rigidbody>().AddForce(Random.Range(-2f, 2f), Random.Range(-2f, 2f), Random.Range(-2f, 2f), ForceMode.Impulse);
+				//item.GetComponent<Rigidbody>().isKinematic = true;
+			}
+		}
+		if (sound) sound.Play();
 
-				Destroy(gameObject);
-    }
+		Destroy(gameObject);
+	}
 }
