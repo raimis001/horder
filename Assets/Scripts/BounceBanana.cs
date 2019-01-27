0cb48e306ae466b4e74cf27eec1675bf75bb7fa1
Ingus Betmans <ingus.betmans@gmail.com>
2019-01-27 12:18:18 +0200

    ss

---
+++ Assets/Scripts/BounceBanana.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceBanana : MonoBehaviour
+{
+    void Update()
+    {
+        if (GetComponent<Rigidbody>().velocity.magnitude < 0.5f)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 3f);
+        }
+    }
+}
